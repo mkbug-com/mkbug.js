2a68478f2646871944cd499665357324d81681a2
梁亚辉 <liangyahui@didiglobal.com>
2020-06-27 13:12:14 +0800

    update

--- test/controller.test.js
+++ test/controller.test.js
@@ -4,7 +4,7 @@ describe("Controller", () => {
   it("初始化", () => {
     let ret = null;
     try {
-      ret = await request.get('http://localhost:3000/heath')
+      ret = await request.get('http://localhost:3000/api/index/test')
     } catch (err) {
       ret = err;
     } finally {
