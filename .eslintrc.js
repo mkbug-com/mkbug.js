7313882b7402c7b0bd0492e03cffcbcd01e14c39
benmo <cky9302@163.com>
2021-09-12 08:14:19 +0800

    feat: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,83 @@
 module.exports = {
   root: true,
   env: {
-    "es6": true,
-    node: true
+    browser: true,
+    node: true,
+    es6: true, // 使用require就不会报错了
+    commonjs: true
   },
+  globals: {
+    describe: true,
+    it: true,
+    expect: true
+  },
+  parser: 'babel-eslint',
+  // 配置解析器
   parserOptions: {
-    parser: 'babel-eslint',
+    sourceType: 'module',
     ecmaVersion: 2017
   },
-  plugins: ["sonarjs"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  // 规则内容参考：http://eslint.cn/docs/rules/
+  // 值为 0 时该规则不生效
+  // 值为 1 时是显示警告
+  // 值为 2 时显示报错
   rules: {
-    "generator-star-spacing": "off", // allow async-await
-    "import/no-webpack-loader-syntax": "off",
-    "no-unused-vars": 1, // 禁止使用未使用的变量
-    "space-before-function-paren": "off",
-    "no-alert": 1, // 不允许使用alert，confirm，prompt
-    "no-delete-var": 1, // 不允许对变量进行delete操作
-    "no-shadow-restricted-names": 1, // 禁止隐藏限制名称
-    "no-caller": 1, // 禁止使用arguments.callee
-    "no-new-symbol": 1, // 不允许对symbol进行new操作
-    "no-void": 1, // 不允许使用void
-    "no-with": 1, // 不允许使用with
-    "for-direction": 1, // 检测for 循环是否正确
-    "no-array-constructor": 1, // 不建议实用 new Array
-    "no-const-assign": 1, // 禁止修改使用const
-    "no-bitwise": 1, // 禁止按位运算符
-    "no-eval": 1, // 不允许使用eval
-    "no-debugger": 1, // 不允许使用debugger
-    "no-param-reassign": 1, // 禁止重新分配功能参数
-    "no-empty-pattern": 1, // 禁止使用空的销毁模式
-    "no-extra-semi": 1, // 不允许使用多余的结尾符号
-    "no-dupe-args": 1, // 不允许一个函数有相同的参数名
-    "no-sequences": 1, // 禁用逗号操作符
-    "no-loop-func": 1, // 禁止在循环语句中出现包含不安全引用的函数声明
-    "no-duplicate-imports": 1, // 禁止重复模块导入
-    "no-labels": 1, // 禁用标签语句
-    "no-trailing-spaces": 1, // 禁用行位空白
-    "no-multi-str": 1, // 禁止使用多行字符串
-    "no-octal": 1, // 禁止使用八进制字面量
-    "no-dupe-keys": 1, // 禁止对象字面量中出现重复的key
-    "no-dupe-class-members": 1, // 禁止类成员中出现重复的名字
-    "no-extra-parens": 1, // 禁止不必要的括号
-    "no-template-curly-in-string": 1, // 禁止在常规字符串中出现模板字面量占位符语法
-    "no-nested-ternary": 0, // 禁用嵌套的三元表达式
-    "no-use-before-define": 1, // 禁止在变量定义之前使用它们
-    "no-undef": 2, // 禁止未声明的变量
-    "no-self-assign": 1, // 禁止自我赋值
-    "no-new-wrappers": 0, // 禁止对 String，Number 和 Boolean 使用 new 操作符
-    "max-len": ["error", {
-      code: 180
-    }], // 强制行的最大长度
-    "max-statements-per-line": ["error", {
-      max: 1
-    }], // 强制每一行中所允许的最大语句数量
-    "max-lines": ["error", 1000], // 文件最大行数
-    "max-lines-per-function": ["error", 200], // 一个函数的最大行数
-    "require-yield": 0, // 要求 generator 函数内有 yield
-    "array-callback-return": 1, // Array return 特别注意 map 和 forEach 的区别
-    "function-paren-newline": 0, // 函数换行符 不建议
-    "constructor-super": 1, // 验证super()in构造函数中的调用
-    "use-isnan": 1, // 使用 isNaN 判断 NaN
-    "no-unused-expressions": 0
-  }
+    // Possible Errors
+    'getter-return': 2,
+    'no-empty': ['warn', { allowEmptyCatch: true }],
+    'use-isnan': 2, // 使用 isNaN 判断 NaN
+    'no-cond-assign': 2, // 禁止条件表达式中出现赋值操作符
+    'no-dupe-keys': 2, // 禁止对象字面量中出现重复的key
+    'no-dupe-args': 2, // 禁止 function 定义中出现重名参数
+    'no-extra-boolean-cast': 2, // 禁止不必要的布尔转换
+    'no-extra-semi': 0, // 禁止不必要的分号
+    'no-regex-spaces': 2, // 禁止正则表达式字面量中出现多个空格
+    'no-irregular-whitespace': ['error', { skipComments: true }],
+
+    // Best Practices
+    'array-callback-return': 2, // Array return 特别注意 map 和 forEach 的区别
+    'no-caller': 2, // 禁止使用arguments.callee
+    'no-alert': 2, // 不允许使用alert，confirm，prompt
+    'no-useless-catch': 1,
+    // 'no-unused-expressions': ["error", { "allowTaggedTemplates": true }], // 禁止出现未使用过的表达式
+
+    // Variables
+    'no-delete-var': 2, // 不允许对变量进行delete操作
+    'no-shadow-restricted-names': 2, // 禁止将标识符定义为受限的名字
+    'no-use-before-define': [2, { functions: false }], // 禁止在变量定义之前使用它们
+    'no-undef': 2, // 禁止未声明的变量
+    'no-unused-vars': [2, { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+
+    // Node.js and CommonJS
+    'no-process-exit': 'off',
+    'no-useless-escape': 'off',
+    // Stylistic Issues
+    'no-trailing-spaces': 2, // 禁用行位空白
+    // 'max-len': ['error', { code: 180 }], // 强制行的最大长度
+    'max-statements-per-line': ['error', { max: 1 }], // 强制每一行中所允许的最大语句数量
+    'max-lines': ['error', 1000], // 文件最大行数
+    'max-lines-per-function': ['error', 200], // 一个函数的最大行数
+    // ECMAScript 6
+    'no-const-assign': 2, // 禁止修改使用const
+    'no-new-symbol': 2,
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'all'
+      }
+    ],
+    'no-duplicate-imports': 2, // 禁止重复模块导入
+    'no-dupe-class-members': 2, // 禁止类成员中出现重复的名字
+    'require-yield': 2, // 要求 generator 函数内有 yield
+    'constructor-super': 2 // 验证super()in构造函数中的调用
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,3 @@
-const types = [
-  'build',
-  'ci',
-  'chore',
-  'docs',
-  'feat',
-  'fix',
-  'pref',
-  'refactor',
-  'revert',
-  'style',
-  'test'
-]
-
-const typeEnum = {
-  rules: {
-    'type-enum': [2, 'always', types]
-  },
-  value: () => types
-}
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
@@ -29,6 +8,6 @@ module.exports = {
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
     'header-max-length': [0, 'always', 72],
-    'type-enum': typeEnum.rules['type-enum']
+    'type-enum': [2, 'always', ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'pref', 'refactor', 'revert', 'style', 'test']]
   }
 }
